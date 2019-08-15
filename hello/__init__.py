508cb9f57015b971e724b79ef9835e7502ca555a
jake23hogs <gilesj@greenbrierschools.org>
2019-08-14 21:15:14 -0500

    Update __init__.py

--- hello/__init__.py
+++ hello/__init__.py
@@ -1,17 +1,20 @@
-import check50
+from check50 import *
 
-@check50.check()
-def exists():
-    """Hello.java exists."""
-    check50.exists("Hello.java")
 
-@check("exists")
+class Hello(Checks):
+    
+    @check()
+    def exists():
+        """Hello.java exists."""
+        self.require("Hello.java")
+
+    @check("exists")
     def compiles(self):
         """Hello compiles"""
         self.spawn("javac Hello.java").exit(0)
 
-@check50.check(compiles)
-def Case1():
-    """Outputs correctly Hello, World!\n."""
-    self.spawn("java Hello").stdout("Hello, World!\n").exit()
+    @check50.check("compiles")
+    def Case1():
+        """Outputs correctly Hello, World!\n."""
+        self.spawn("java Hello").stdout("Hello, World!\n", "Hello, World!\n").exit(0)
 
