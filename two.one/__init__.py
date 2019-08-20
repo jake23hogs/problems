2d22a52cb3700038e6675fcece4d13a60ca532ed
jake23hogs <gilesj@greenbrierschools.org>
2019-08-19 22:38:13 -0500

    Create __init__.py

---
+++ two.one/__init__.py
@@ -0,0 +1,49 @@
+import check50
+
+@check50.check()
+def exists(self):
+    """Fahrenheit.java exists"""
+    self.require("Fahrenheit.java")
+
+@check50.check(exists)
+def compiles(self):
+    """Fahrenheit.java compiles"""
+    self.spawn("javac Fahrenheit.java").exit(0)
+
+@check50.check("compiles")
+def test37(self):
+    """37 degrees Celsius yields 98.6 degrees Fahrenheit"""
+    self.spawn("java Fahrenheit").stdin("37").stdout(number(98.6), "98.6\n").exit(0)
+
+@check50.check("compiles")
+def test0(self):
+    """0 degrees Celsius yields 32.0 degrees Fahrenheit"""
+    self.spawn("java Fahrenheit").stdin("0").stdout(number(32.0), "32.0\n").exit(0)
+
+@check50("compiles")
+def test100(self):
+    """100.00 degrees Celsius yields 212.0 degrees Fahrenheit"""
+    self.spawn("java Fahrenheit").stdin("100.00").stdout(number(212.0), "212.0\n").exit(0)
+
+@check50("compiles")
+def testneg40(self):
+    """-40 degrees Celsius yields -40.0 degrees Fahrenheit"""
+    self.spawn("java Fahrenheit").stdin("-40").stdout(number(-40.0), "-40.0\n").exit(0)
+
+@check50("compiles")
+def test18point5(self):
+    """18.5 degrees Celsius yields 65.3 degrees Fahrenheit"""
+    self.spawn("java Fahrenheit").stdin("18.5").stdout(number(65.3), "65.3\n").exit(0)
+
+@check50("compiles")
+def testneg123point45678(self):
+    """-123.45678 degrees Celsius yields -190.222204 degrees Fahrenheit"""
+    self.spawn("java Fahrenheit").stdin("-123.45678").stdout(number(-190.222204), "-190.222204\n").exit(0)
+
+@check50("compiles")
+def test_reject_empty_string(self):
+    """rejects a non-numeric input of "" """
+    self.spawn("java Fahrenheit").stdin("").reject()
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
