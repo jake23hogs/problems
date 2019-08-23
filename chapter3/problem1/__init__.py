c4b52a2ff02523ac2c3dd37eb8a32a3da89fb5f9
jake23hogs <gilesj@greenbrierschools.org>
2019-08-23 09:43:59 -0500

    Create __init__.py

---
+++ chapter3/problem1/__init__.py
@@ -0,0 +1,25 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+    """Project3_1.java exists"""
+    check50.exists("Project3_1.java")
+
+@check50.check(exists)
+def test2():
+    """2 yields a surface area of 8 """
+    check50.run("java Project3_1").stdin("2").stdout(number(8), "8\n").exit(0)
+
+@check50.check(exists)
+def test0():
+    """0 yields a surface area of 0"""
+    check50.run("java Project3_1").stdin("0").stdout(number(0), "0\n").exit(0)
+
+@check50.check(exists)
+def test_reject_empty_string():
+    """rejects a non-numeric input of "" """
+    check50.run("java Project3_1").stdin("").reject()
+
+def number(num):
+    return "(^|[^\d]){}[^\d]".format(num)
