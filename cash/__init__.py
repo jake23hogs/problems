9d6cbf4bc50ece7985a716790ea0961d3569a0d9
jake23hogs <gilesj@greenbrierschools.org>
2019-09-09 07:57:23 -0500

    Update __init__.py

--- cash/__init__.py
+++ cash/__init__.py
@@ -10,37 +10,37 @@ def exists():
 @check50.check(exists)
 def compiles():
     """cash compiles"""
-    check50.c.compile("cash.c", lcs50=True)
+    check50.run("javac Cash.java")
 
 
 @check50.check(compiles)
 def test041():
     """input of 0.41 yields output of 4"""
-    check50.run("./cash").stdin("0.41").stdout(coins(4), "4\n").exit(0)
+    check50.run("java Cash").stdin("0.41").stdout(coins(4), "4\n").exit(0)
 
 
 @check50.check(compiles)
 def test001():
     """input of 0.01 yields output of 1"""
-    check50.run("./cash").stdin("0.01").stdout(coins(1), "1\n").exit(0)
+    check50.run("java Cash").stdin("0.01").stdout(coins(1), "1\n").exit(0)
 
 
 @check50.check(compiles)
 def test015():
     """input of 0.15 yields output of 2"""
-    check50.run("./cash").stdin("0.15").stdout(coins(2), "2\n").exit(0)
+    check50.run("java Cash").stdin("0.15").stdout(coins(2), "2\n").exit(0)
 
 
 @check50.check(compiles)
 def test160():
     """input of 1.6 yields output of 7"""
-    check50.run("./cash").stdin("1.6").stdout(coins(7), "7\n").exit(0)
+    check50.run("java Cash").stdin("1.6").stdout(coins(7), "7\n").exit(0)
 
 
 @check50.check(compiles)
 def test230():
     """input of 23 yields output of 92"""
-    check50.run("./cash").stdin("23").stdout(coins(92), "92\n").exit(0)
+    check50.run("java Cash").stdin("23").stdout(coins(92), "92\n").exit(0)
 
 
 @check50.check(compiles)
@@ -48,7 +48,7 @@ def test420():
     """input of 4.2 yields output of 18"""
     from re import search
     expected = "18\n"
-    actual = check50.run("./cash").stdin("4.2").stdout()
+    actual = check50.run("java Cash").stdin("4.2").stdout()
     if not search(coins(18), actual):
         help = None
         if search(coins(22), actual):
@@ -59,19 +59,19 @@ def test420():
 @check50.check(compiles)
 def test_reject_negative():
     """rejects a negative input like -1"""
-    check50.run("./cash").stdin("-1").reject()
+    check50.run("java Cash").stdin("-1").reject()
 
 
 @check50.check(compiles)
 def test_reject_foo():
     """rejects a non-numeric input of "foo" """
-    check50.run("./cash").stdin("foo").reject()
+    check50.run("java Cash").stdin("foo").reject()
 
 
 @check50.check(compiles)
 def test_reject_empty():
     """rejects a non-numeric input of "" """
-    check50.run("./cash").stdin("").reject()
+    check50.run("java Cash").stdin("").reject()
 
 
 def coins(num):
