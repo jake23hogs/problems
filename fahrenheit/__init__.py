40d16c167a5f20e5931299d3beac2230c00aaf81
jake23hogs <jgiles@paragouldschools.net>
2020-09-29 10:08:58 -0500

    Create __init__.py

---
+++ fahrenheit/__init__.py
@@ -0,0 +1,20 @@
+import check50
+
+
+@check50.check()
+def exists():
+    """Fahrenheit exists."""
+    check50.exists("Fahrenheit.java")
+
+    
+@check50.check(exists)
+def compiles():
+    """Fahrenheit compiles."""
+    check50.run("javac Fahrenheit.java")
+
+    
+@check50.check(compiles)
+def correct conversion():
+    """converts 0 C to 32 F"""
+    check50.run("java Fahrenheit").stdin(0).stdout(32.0, 32.0).exit(0)
+
