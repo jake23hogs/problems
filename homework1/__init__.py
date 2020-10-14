29875f577cb95aad39c44e72dfdb486f34582bc8
jake23hogs <jgiles@paragouldschools.net>
2020-10-14 12:46:16 -0500

    Create __init__.py

---
+++ homework1/__init__.py
@@ -0,0 +1,195 @@
+import check50
+
+
+@check50.check()
+def exists1():
+    """program1 exists"""
+    check50.exists("Program1.java")
+
+
+@check50.check(exists1)
+def compiles1():
+    """program1 compiles"""
+    check50.run("javac Program1")
+
+@check50.check()
+def exists2():
+    """program2 exists"""
+    check50.exists("Program2.java")
+
+
+@check50.check(exists2)
+def compiles2():
+    """program2 compiles"""
+    check50.run("javac Program2")
+    
+@check50.check()
+def exists3():
+    """program3 exists"""
+    check50.exists("Program3.java")
+
+
+@check50.check(exists3)
+def compiles3():
+    """program3 compiles"""
+    check50.run("javac Program3")
+
+
+@check50.check()
+def exists4():
+    """program4 exists"""
+    check50.exists("Program4.java")
+
+
+@check50.check(exists4)
+def compiles4():
+    """program4 compiles"""
+    check50.run("javac Program4")
+    
+@check50.check()
+def exists5():
+    """program5 exists"""
+    check50.exists("Program5.java")
+
+
+@check50.check(exists5)
+def compiles5():
+    """program5 compiles"""
+    check50.run("javac Program5")
+
+
+@check50.check()
+def exists6():
+    """program6 exists"""
+    check50.exists("Program6.java")
+
+
+@check50.check(exists6)
+def compiles6():
+    """program6 compiles"""
+    check50.run("javac Program6")
+
+@check50.check()
+def exists7():
+    """program7 exists"""
+    check50.exists("Program7.java")
+
+
+@check50.check(exists7)
+def compiles7():
+    """program7 compiles"""
+    check50.run("javac Program7")
+
+
+@check50.check()
+def exists8():
+    """program8 exists"""
+    check50.exists("Program8.java")
+
+
+@check50.check(exists8)
+def compiles8():
+    """program8 compiles"""
+    check50.run("javac Program8")
+    
+@check50.check()
+def exists9():
+    """program9 exists"""
+    check50.exists("Program9.java")
+
+
+@check50.check(exists9)
+def compiles9():
+    """program9 compiles"""
+    check50.run("javac Program9")
+
+@check50.check()
+def exists10():
+    """program10 exists"""
+    check50.exists("Program10.java")
+
+
+@check50.check(exists10)
+def compiles10():
+    """program10 compiles"""
+    check50.run("javac Program10")
+
+
+@check50.check()
+def exists11():
+    """program11 exists"""
+    check50.exists("Program11.java")
+
+
+@check50.check(exists11)
+def compiles11():
+    """program11 compiles"""
+    check50.run("javac Program11")
+    
+@check50.check()
+def exists12():
+    """program12 exists"""
+    check50.exists("Program12.java")
+
+
+@check50.check(exists12)
+def compiles12():
+    """program12 compiles"""
+    check50.run("javac Program12")
+
+@check50.check()
+def exists13():
+    """program13 exists"""
+    check50.exists("Program13.java")
+
+
+@check50.check(exists13)
+def compiles13():
+    """program13 compiles"""
+    check50.run("javac Program13")
+
+
+@check50.check()
+def exists14():
+    """program14 exists"""
+    check50.exists("Program14.java")
+
+
+@check50.check(exists14)
+def compiles14():
+    """program14 compiles"""
+    check50.run("javac Program14")
+
+@check50.check()
+def exists15():
+    """program15 exists"""
+    check50.exists("Program15.java")
+
+
+@check50.check(exists15)
+def compiles15():
+    """program15 compiles"""
+    check50.run("javac Program15")
+
+
+@check50.check()
+def exists16():
+    """program16 exists"""
+    check50.exists("Program16.java")
+
+
+@check50.check(exists16)
+def compiles16():
+    """program16 compiles"""
+    check50.run("javac Program16")
+    
+@check50.check()
+def exists17():
+    """program17 exists"""
+    check50.exists("Program17.java")
+
+
+@check50.check(exists17)
+def compiles17():
+    """program17 compiles"""
+    check50.run("javac Program17")
