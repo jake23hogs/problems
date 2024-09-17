3e774936484822b3b47771db75c272ca4b2e4580
jake23hogs <jgiles@paragouldschools.net>
2024-09-17 09:30:05 -0500

    Update __init__.py

--- classwork2/__init__.py
+++ classwork2/__init__.py
@@ -3,157 +3,159 @@ import check50
 
 @check50.check()
 def exists1():
-    """program1 exists"""
-    check50.exists("program1.c")
+    """Program1 exists"""
+    check50.exists("Program1.java")
 
 
 @check50.check(exists1)
 def compiles1():
-    """program1 compiles"""
-    check50.run("make program1")
-
-@check50.check(compiles1)
-def Example1(self):
-    """Program1 Test 1"""
-    check50.run("./program1").stdin("17").stdout("You are not old enough to vote!\n" , "You are not old enough to vote!\n").exit(0)
-
-@check50.check(compiles1)
-def Example2(self):
-    """Program1 Test 2"""
-    check50.run("./program1").stdin("18").stdout("You are old enough to vote!\n" , "You are old enough to vote!\n").exit(0)
+    """Program1 compiles"""
+    check50.run("javac Program1.java")
 
 @check50.check()
 def exists2():
-    """program2 exists"""
-    check50.exists("program2.c")
+    """Program2 exists"""
+    check50.exists("Program2.java")
 
 
 @check50.check(exists2)
 def compiles2():
-    """program2 compiles"""
-    check50.run("make program2")
+    """Program2 compiles"""
+    check50.run("javac Program2.java")
     
 @check50.check()
 def exists3():
-    """program3 exists"""
-    check50.exists("program3.c")
+    """Program3 exists"""
+    check50.exists("Program3.java")
 
 
 @check50.check(exists3)
 def compiles3():
-    """program3 compiles"""
-    check50.run("make program3")
+    """Program3 compiles"""
+    check50.run("javac Program3.java")
 
 
 @check50.check()
 def exists4():
-    """program4 exists"""
-    check50.exists("program4.c")
+    """Program4 exists"""
+    check50.exists("Program4.java")
 
 
 @check50.check(exists4)
 def compiles4():
-    """program4 compiles"""
-    check50.run("make program4")
+    """Program4 compiles"""
+    check50.run("javac Program4.java")
     
 @check50.check()
 def exists5():
-    """program5 exists"""
-    check50.exists("program5.c")
+    """Program5 exists"""
+    check50.exists("Program5.java")
 
 
 @check50.check(exists5)
 def compiles5():
-    """program5 compiles"""
-    check50.run("make program5")
+    """Program5 compiles"""
+    check50.run("javac Program5.java")
 
 
 @check50.check()
 def exists6():
-    """program6 exists"""
-    check50.exists("program6.c")
+    """Program6 exists"""
+    check50.exists("Program6.java")
 
 
 @check50.check(exists6)
 def compiles6():
-    """program6 compiles"""
-    check50.run("make program6")
+    """Program6 compiles"""
+    check50.run("javac Program6.java")
 
 @check50.check()
 def exists7():
-    """program7 exists"""
-    check50.exists("program7.c")
+    """Program7 exists"""
+    check50.exists("Program7.java")
 
 
 @check50.check(exists7)
 def compiles7():
-    """program7 compiles"""
-    check50.run("make program7")
+    """Program7 compiles"""
+    check50.run("javac Program7.java")
 
 
 @check50.check()
 def exists8():
-    """program8 exists"""
-    check50.exists("program8.c")
+    """Program8 exists"""
+    check50.exists("Program8.java")
 
 
 @check50.check(exists8)
 def compiles8():
-    """program8 compiles"""
-    check50.run("make program8")
+    """Program8 compiles"""
+    check50.run("javac Program8.java")
     
 @check50.check()
 def exists9():
-    """program9 exists"""
-    check50.exists("program9.c")
+    """Program9 exists"""
+    check50.exists("Program9.java")
 
 
 @check50.check(exists9)
 def compiles9():
-    """program9 compiles"""
-    check50.run("make program9")
+    """Program9 compiles"""
+    check50.run("javac Program9.java")
 
 @check50.check()
 def exists10():
-    """program10 exists"""
-    check50.exists("program10.c")
+    """Program10 exists"""
+    check50.exists("Program10.java")
 
 
 @check50.check(exists10)
 def compiles10():
-    """program10 compiles"""
-    check50.run("make program10")
+    """Program10 compiles"""
+    check50.run("javac Program10.java")
 
 
 @check50.check()
 def exists11():
-    """program11 exists"""
-    check50.exists("program11.c")
+    """Program11 exists"""
+    check50.exists("Program11.java")
 
 
 @check50.check(exists11)
 def compiles11():
-    """program11 compiles"""
-    check50.run("make program11")
+    """Program11 compiles"""
+    check50.run("javac Program11.java")
     
 @check50.check()
 def exists12():
-    """program12 exists"""
-    check50.exists("program12.c")
+    """Program12 exists"""
+    check50.exists("Program12.java")
 
 
 @check50.check(exists12)
 def compiles12():
-    """program12 compiles"""
-    check50.run("make program12")
+    """Program12 compiles"""
+    check50.run("javac Program12.java")
 
 @check50.check()
 def exists13():
-    """program13 exists"""
-    check50.exists("program13.c")
+    """Program13 exists"""
+    check50.exists("Program13.java")
 
 
 @check50.check(exists13)
 def compiles13():
-    """program13 compiles"""
-    check50.run("make program13")
+    """Program13 compiles"""
+    check50.run("javac Program13.java")
+
+
+@check50.check()
+def exists14():
+    """Program14 exists"""
+    check50.exists("Program14.java")
+
+
+@check50.check(exists14)
+def compiles14():
+    """Program14 compiles"""
+    check50.run("javac Program14.java")
